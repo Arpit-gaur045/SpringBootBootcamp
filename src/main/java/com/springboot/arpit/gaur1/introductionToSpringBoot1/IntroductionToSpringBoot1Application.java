9424519e7ed8a90b00ee597123845e0d143bf697
Arpit-gaur045 <arpitgaur045@gmail.com>
2025-11-16 13:59:05 +0530

    setting up Springboot

---
+++ src/main/java/com/springboot/arpit/gaur1/introductionToSpringBoot1/IntroductionToSpringBoot1Application.java
@@ -0,0 +1,16 @@
+package com.springboot.arpit.gaur1.introductionToSpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBoot1Application {
+
+	public static void main(String[] args) {
+
+//        System.out.println("Hello World");
+//        System.out.println("Hello World");
+		SpringApplication.run(IntroductionToSpringBoot1Application.class, args);
+	}
+
+}
