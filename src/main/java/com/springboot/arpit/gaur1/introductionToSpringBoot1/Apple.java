d6e64c675b00c19aa30e648c2dc989449b6602f8
Arpit-gaur045 <arpitgaur045@gmail.com>
2025-11-16 15:31:07 +0530

    Injecting Beans using Stereotype Annotation @Component

---
+++ src/main/java/com/springboot/arpit/gaur1/introductionToSpringBoot1/Apple.java
@@ -0,0 +1,16 @@
+package com.springboot.arpit.gaur1.introductionToSpringBoot1;
+
+import org.springframework.stereotype.Component;
+
+   @Component
+    public class Apple {
+
+       void getApple(){
+            System.out.println("Apple is here");
+        }
+
+    }
+
+
+
+
--- src/main/java/com/springboot/arpit/gaur1/introductionToSpringBoot1/IntroductionToSpringBoot1Application.java
+++ src/main/java/com/springboot/arpit/gaur1/introductionToSpringBoot1/IntroductionToSpringBoot1Application.java
@@ -1,16 +1,26 @@
 package com.springboot.arpit.gaur1.introductionToSpringBoot1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBoot1Application {
+public class IntroductionToSpringBoot1Application implements CommandLineRunner {
+
+    @Autowired
+    private Apple apple;
 
 	public static void main(String[] args) {
 
-//        System.out.println("Hello World");
-//        System.out.println("Hello World");
+
 		SpringApplication.run(IntroductionToSpringBoot1Application.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        apple.getApple();
+
+    }
 }
